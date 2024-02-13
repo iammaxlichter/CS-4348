3f7c52caca856ab90250a8b6ed88fb32661e2566
Max Lichter <iammaxlichter@gmail.com>
2024-02-12 23:00:58 -0600

    call and return address

--- Project1.java
+++ Project1.java
@@ -1,9 +1,10 @@
 import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Project1 {
-    int[] Memory = new int[2000];
+    int[] Memory = new int[20];
 
     public void ParentFunction() {
         int PC = 0, SP = 0, IR = 0, AC = 0;
@@ -13,9 +14,9 @@ public class Project1 {
 
     public void Switch(int PC, int SP, int IR, int AC, int X, int Y) {
         int i = 0;
-        while (i < Memory.length) {
+        int returnAddress = 0;
+        while (i < Memory.length && Memory[i] != 0) {
             String line = Integer.toString(Memory[i]).trim();
-
             String[] parts = line.split("//");
             if (parts.length > 0) {
                 line = parts[0].trim();
@@ -26,102 +27,109 @@ public class Project1 {
                 continue;
             }
 
+            
             switch (line) {
-                case "1": //Load Value
+                case "1": // Load Value
+                
                     if (++i < Memory.length) {
                         AC = Memory[i];
                     }
                     break;
 
-                case "8": //Get 
+                case "8": // Get
                     int randomNumber = new Random().nextInt(100) + 1;
                     AC = randomNumber;
                     break;
 
-                case "9": //Put
+                    case "9": // Put
                     if (++i < Memory.length) {
                         String nextLine = Integer.toString(Memory[i]);
+                        //System.out.println("\n" + nextLine + " " + AC);
+                        
                         switch (nextLine) {
                             case "1":
                                 System.out.print(AC);
                                 break;
                             case "2":
                                 System.out.print((char) AC);
+                                
                                 break;
                             default:
-                                
+                                // Handle other cases if necessary
                         }
                     }
                     break;
 
-                case "10": //AddX
+                case "10": // AddX
                     AC = AC + X;
                     break;
 
-                case "11": //AddY
+                case "11": // AddY
                     AC = AC + Y;
                     break;
 
-                case "12": //SubX
+                case "12": // SubX
                     AC = AC - X;
                     break;
 
-                case "13": //SubY
+                case "13": // SubY
                     AC = AC - Y;
                     break;
 
-                case "14": //CopyToX
+                case "14": // CopyToX
                     X = AC;
                     break;
 
-                case "15": //CopyFromX
+                case "15": // CopyFromX
                     AC = X;
                     break;
-                    
-                case "16": //CopyToY
+
+                case "16": // CopyToY
                     Y = AC;
                     break;
-                
-                case "17": //CopyFromY
+
+                case "17": // CopyFromY
                     AC = Y;
                     break;
-                
-                case "18": //CopyToSP
+
+                case "18": // CopyToSP
                     SP = AC;
                     break;
 
-                case "19": //CopyFromSP
+                case "19": // CopyFromSP
                     AC = SP;
                     break;
 
-                case "23": //call address
-  
+                case "23": // call address
+                    if (++i < Memory.length) {
+                        returnAddress = i + 1; // Store the return address
+                        i = Memory[i] - 1; // Jump to the specified address
+                    }
                     break;
 
-                case "24": //return address
-
+                case "24": // return address
+                    i = returnAddress - 1; // Jump back to the return address
                     break;
 
-                case "25": //IncX
+                case "25": // IncX
                     X++;
                     break;
 
-                case "26": //DecX
+                case "26": // DecX
                     X--;
                     break;
 
-                case "27": //Push
+                case "27": // Push
                     break;
 
-                case "28": //Pop
+                case "28": // Pop
                     break;
 
-                case "29": //Int System Call
-                    break;
-            
-                case "30": //IRet Return from system call
+                case "29": // Int System Call
                     break;
 
+                case "30": // IRet Return from system call
+                    break;
 
                 case "50":
                     return;
@@ -130,6 +138,7 @@ public class Project1 {
                     System.out.println("Not a recognized command: " + line);
             }
             i++;
+            
         }
     }
 
@@ -159,6 +168,8 @@ public class Project1 {
         }
     }
 
+   
+
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: java Project1 <input_file>");
@@ -169,4 +180,5 @@ public class Project1 {
         Project1 project = new Project1();
         project.FileRead(fileName);
     }
+
 }
